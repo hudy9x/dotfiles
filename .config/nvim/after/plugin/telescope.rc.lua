abcf2b25875526a0ad57a4f349c420b3ecc33d0d
hudy9x <hudy9x@gmail.com>
2022-09-14 15:58:40 +0700

    fix: using new syntax for bufferline

--- .config/nvim/after/plugin/telescope.rc.lua
+++ .config/nvim/after/plugin/telescope.rc.lua
@@ -11,6 +11,7 @@ local fb_actions = require "telescope".extensions.file_browser.actions
 
 telescope.setup {
   defaults = {
+    file_ignore_patterns = { "node_modules", "\\.git", "src-tauri\\\\target\\\\debug", "\\.next" },
     mappings = {
       n = {
         ["q"] = actions.close
