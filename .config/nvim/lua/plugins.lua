7c8f946997b34fb27964a3d0a02ca17eaf5be856
hudy9x <hudy9x@gmail.com>
2023-02-07 20:39:40 +0700

    feat: add diffview plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -47,6 +47,7 @@ packer.startup(function(use)
 
   use 'lewis6991/gitsigns.nvim'
   use 'dinhhuy258/git.nvim'
+  use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
 
   use {
     "folke/trouble.nvim",
