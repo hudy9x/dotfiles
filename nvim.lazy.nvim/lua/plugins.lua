ba7166997f041f1174ee016e387f39312436089a
hudy@mac-air <hudy9x@gmail.com>
2023-05-25 00:28:40 +0700

    add completion

--- nvim.lazy.nvim/lua/plugins.lua
+++ nvim.lazy.nvim/lua/plugins.lua
@@ -14,7 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 local theme = require('theme')
 
 -- PLugins load
-require("lazy").setup({  
+require("lazy").setup({
 	theme,
 	-- Auto pairs {}, '', "", ()
 	{
@@ -94,6 +94,22 @@ require("lazy").setup({
 			-- Useful status updates for LSP
 			{ 'j-hui/fidget.nvim', opts = {} }
 		}
-	}
+	},
+
+	{
+		-- Autocompletion
+		'hrsh7th/nvim-cmp',
+		dependencies = {
+			-- Snippet Engine & its associated nvim-cmp source
+			'L3MON4D3/LuaSnip',
+			'saadparwaiz1/cmp_luasnip',
+
+			-- Adds LSP completion capabilities
+			'hrsh7th/cmp-nvim-lsp',
+
+			-- Adds a number of user-friendly snippets
+			'rafamadriz/friendly-snippets',
+		},
+  },
 
 })
