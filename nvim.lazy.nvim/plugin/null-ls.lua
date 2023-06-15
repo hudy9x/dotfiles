fadf0648498ec160e1b775b7917710811b1d82b8
hudy9x <hudy9x@gmail.com>
2023-06-15 16:14:25 +0700

    use eslint_d for formatting

--- nvim.lazy.nvim/plugin/null-ls.lua
+++ nvim.lazy.nvim/plugin/null-ls.lua
@@ -1,8 +1,9 @@
 local ok, null_ls = pcall(require, 'null-ls')
 if not ok then return end
 
--- null_ls.setup({
--- 	sources = {
--- null_ls.builtins.diagnostics.eslint_d
--- 	}
--- })
+null_ls.setup({
+	debug = true,
+	sources = {
+		null_ls.builtins.formatting.eslint_d
+	}
+})
