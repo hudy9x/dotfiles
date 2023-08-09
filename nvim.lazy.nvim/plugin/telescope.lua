6c3bf9f009812cd73ac8e3483d38f7fae3fbf789
hudy9x <hudy9x@gmail.com>
2023-08-09 18:06:53 +0700

    refactor(telescope): using center layout

--- nvim.lazy.nvim/plugin/telescope.lua
+++ nvim.lazy.nvim/plugin/telescope.lua
@@ -10,3 +10,11 @@ vim.keymap.set('n', ';gbr', builtin.git_branches)
 vim.keymap.set('n', ';gst', builtin.git_status)
 vim.keymap.set('n', ';gcm', builtin.git_commits)
 vim.keymap.set('n', ';xx', builtin.diagnostics)
+
+telescope.setup {
+  defaults = {
+    layout_strategy = 'center',
+    layout_config = { height = 0.75 }
+  }
+
+}
