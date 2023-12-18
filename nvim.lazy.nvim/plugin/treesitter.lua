7e033b18be6ec73c86a58c065848e43dd7c08b5e
hudy9x <hudy9x@gmail.com>
2023-12-18 09:31:02 +0700

    remove redundance treesitter file

--- nvim.lazy.nvim/plugin/treesitter.lua
+++
@@ -1,9 +0,0 @@
-local stt, treesitter = pcall(require, 'nvim-treesitter.configs')
-if not stt then return end
-
-treesitter.setup {
-  ensure_installed = {'lua', 'rust', 'tsx', 'typescript', 'vim', 'prisma' },
-  highlight = { enable = true },
-  indent = { endable = true },
-  auto_install = false
-}
