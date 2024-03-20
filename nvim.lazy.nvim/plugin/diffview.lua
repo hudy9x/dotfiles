52b912a322f98fdfbb897d3b8b5c112c7f0a71c5
hudy9x <hudy9x@gmail.com>
2024-03-20 08:36:43 +0700

    set merge view to diff3_mixed

--- nvim.lazy.nvim/plugin/diffview.lua
+++ nvim.lazy.nvim/plugin/diffview.lua
@@ -2,4 +2,10 @@ local stt, diffview = pcall(require, 'diffview')
 
 if not stt then return end
 
-diffview.setup({})
+diffview.setup({
+  view = {
+    merge_tool = {
+      layout = "diff3_mixed"
+    }
+  }
+})
